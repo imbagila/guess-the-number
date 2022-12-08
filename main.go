069e77e2360a18928b77dee8b0f7e153b7e4c162
imbagila <roid.rabbani@gmail.com>
2022-12-08 22:53:58 +0700

    feat: add guess game

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var guess int
+var answer int
+
 func main() {
-	println("hello world")
+	rand.Seed(time.Now().UnixNano())
+	play()
+}
+
+func play() {
+	randomNumber()
+	inputNumber()
+}
+
+func randomNumber() {
+	answer = rand.Intn(100)
+}
+
+func inputNumber() {
+	fmt.Print("Guess the number: ")
+	_, err := fmt.Scanln(&guess)
+	if err != nil {
+		inputNumber()
+	}
+	check()
+}
+
+func check() {
+	switch {
+	case guess > answer:
+		fmt.Println("Too big !")
+		inputNumber()
+	case guess < answer:
+		fmt.Println("Too small !")
+		inputNumber()
+	default:
+		fmt.Println("Correct !")
+	}
 }
